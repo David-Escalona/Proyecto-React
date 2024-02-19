a5f8a72adcfc9f0cc4f3d53e268c9d0459eacc8d
David-Escalona <escalonagarciadavid@fpllefia.com>
2024-02-19 19:51:50 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from '/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -15,8 +15,9 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        
       </div>
-      <h1>Vite + React</h1>
+      <h1>David Escalona García</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
