34e79d9cb4b3f8dffb8c41f783edfdb857c9a28a
David-Escalona <120495685+David-Escalona@users.noreply.github.com>
2024-02-26 19:30:54 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,28 @@
-import { useState } from 'react'
-import reactLogo from '/react.svg'
-import viteLogo from '/vite.svg'
-import tailwingLogo from '/tailwing.svg'
+import { Animal } from './Animal';
+import { Titulo } from './componentes/header.jsx';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://tailwindcss.com/" target="_blank">
-          <img src={tailwingLogo} className="logo tailwing" alt="tailwing logo" />
-        </a>
-        
+  console.log(bd);
+  
+    return (
+      <div className="p-5 flex flex-col items-center">
+        <Titulo />
+        <div className="flex gap-3 mt-5">
+          {
+            bd.map((animal, index)=>{
+              return(
+                <Animal key = {index} nombre={animal.nombre} imagen={animal.imagen}/>
+              )
+            })
+  
+          }
+        </div>
+  
       </div>
-      <h1>David Escalona García</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    )
+  }
+  
+  export default App
+  
--- src/main.jsx
+++ src/main.jsx
@@ -3,20 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-import { main } from './componentes/main.js'
-import { footer } from './componentes/footer.js'
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
-
-//Inyectamos el componente header
-document.querySelector('header').innerHTML = header.template
-
-//Inyectamos el componente main
-document.querySelector('main').innerHTML = main.template
-
-//Inyectamos el componente footer
-document.querySelector('footer').innerHTML = footer.template
