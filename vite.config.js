efca5c13aed78689fe5297c6afef378e39a97e14
David-Escalona <120495685+David-Escalona@users.noreply.github.com>
2024-04-04 06:42:21 +0000

    commit

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // Definimos la carpeta donde buscar cuando se carga la aplicación (donde estará el main.js)
-})
