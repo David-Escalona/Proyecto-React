f5d09ca07662987e391fcc6acb5ac759f44fff24
David-Escalona <120495685+David-Escalona@users.noreply.github.com>
2024-04-04 10:53:52 +0000

    Despliegue

--- my-react-app/src/componentes/About.jsx
+++ my-react-app/src/componentes/About.jsx
@@ -9,9 +9,37 @@ function About(){
                     <p>Here you will find more information about me, what I do, and my current skills mostly in terms</p>
                     <p>of programming and technology</p>
                 </div>
+                <div className="columns-2 flex justify-center ms-12 ps-12">
+                    <div className="text-start w-full">
+                        <h3 className="text-3xl font-extrabold">Get to know me!</h3>
+                        <div className="py-10 mr-56 text-gray-600 text-lg">
+                            <p>I´m a <strong>Frontend Focused Web Developer</strong> building and managing the Front-end of Websites and Web Applications that leads to the success of the overall product. Check out some of my work in the <strong>Projects</strong> section.</p>
+                            <p className="py-3">I also like sharing content related to the stuff that I have learned over the years in <strong>Web Development</strong> so it can help other people of the Dev Community. Feel free to Connect or Follow me on my <a href="#" className="text-purple-600 decoration-purple-700 underline "><strong>Linkedin</strong></a> and <a href="#" className="text-purple-600 decoration-purple-700 underline "><strong>Instagram</strong></a> where I post useful content related to Web Development and Programming</p>
+                            <p>I´m open to <strong>Job</strong> opportunities where I can contribute, learn and grow. If you have a good opportunity that matches my skills and experience then don´t hesitate to <strong>contact</strong> me.</p>
+                        </div>
+                        <a href="#contact" className="bg-violet-600 py-3 px-10 rounded-md uppercase text-white font-bold text-base shadow-xl tracking-wider">contact</a>
+                    </div>
+                    <div className="text-start w-full">
+                        <h3 className="text-3xl font-extrabold">My Skills</h3>
+                        <div className="flex flex-wrap gap-3 mt-8 pr-20">
+                            <div className="bg-zinc-200 p-3 px-4 rounded text-gray-500 text-center font-bold">HTML</div>
+                            <div className="bg-zinc-200 p-3 px-4 rounded text-gray-500 text-center font-bold">CSS</div>
+                            <div className="bg-zinc-200 p-3 px-4 rounded text-gray-500 text-center font-bold">JavaScript</div>
+                            <div className="bg-zinc-200 p-3 px-4 rounded text-gray-500 text-center font-bold">React</div>
+                            <div className="bg-zinc-200 p-3 px-4 rounded text-gray-500 text-center font-bold">Wordpress</div>
+                            <div className="bg-zinc-200 p-3 px-4 rounded text-gray-500 text-center font-bold">PHP</div>
+                            <div className="bg-zinc-200 p-3 px-4 rounded text-gray-500 text-center font-bold">SASS</div>
+                            <div className="bg-zinc-200 p-3 px-4 rounded text-gray-500 text-center font-bold">GIT</div>
+                            <div className="bg-zinc-200 p-3 px-4 rounded text-gray-500 text-center font-bold">Github</div>
+                            <div className="bg-zinc-200 p-3 px-4 rounded text-gray-500 text-center font-bold">Responsive Design</div>
+                            <div className="bg-zinc-200 p-3 px-4 rounded text-gray-500 text-center font-bold">SEO</div>
+                            <div className="bg-zinc-200 p-3 px-4 rounded text-gray-500 text-center font-bold">Terminal</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
 }
-  
+
 export default About
--- my-react-app/src/componentes/templates/Chat.jsx
+++ my-react-app/src/componentes/templates/Chat.jsx
@@ -2,8 +2,8 @@ function Chat(){
     return (
         
         <div className="fixed bottom-4 right-4 flex justify-between items-center w-70">
-            <p className="text-dark shadow-md rounded-full p-3">Chat with me 👋</p>
-            <div className="p-1.5 my-1 rounded-full bg-red hover:bg-white-100"><img className="h-10 w-10 rounded-full" src="https://www.rammaheshwari.com/assets/svg/linkedin-dark.svg" alt="Linkedin"/></div>
+            <p className="text-dark shadow-md rounded-full p-3 bg-white">Chat with me 👋</p>
+            <div  className="ms-3 p-1.5 my-1 rounded-full bg-violet-600 hover:bg-white transition duration-2000"><img className="h-10 w-10 rounded-full" src="https://www.rammaheshwari.com/assets/svg/linkedin-dark.svg" alt="Linkedin"/></div>
         </div>
 
     )
