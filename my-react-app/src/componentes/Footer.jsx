d66407afc2083de1564d65b36b522e1a743766c4
David-Escalona <120495685+David-Escalona@users.noreply.github.com>
2024-04-08 08:48:03 +0000

    commit

--- my-react-app/src/componentes/Footer.jsx
+++ my-react-app/src/componentes/Footer.jsx
@@ -5,26 +5,26 @@ function Footer(){
         
         <div className="bg-black text-white p-12">
             <div className="columns-2 mt-12">
-                <div className="flex justify-around text-start">
+                <div className="flex justify-start text-start flex-start">
                     <h1 className="text-3xl">DAVID ESCALONA GARCÍA</h1>
                 </div>
-                <div className="flex justify-around">
+                <div className="flex justify-end text-end flex-end">
                     <p className="text-3xl">SOCIAL</p>
                 </div>
             </div>
 
             <div className="columns-2 mt-5">
-                <div className="text-start flex justify-around w-full">
-                    <p className="text-start flex justify-around w-full">A Frontend focused Web Developer building the Frontend of Websites and</p>
+                <div className="flex justify-start text-start flex-start">
+                    <p className="flex justify-start text-start flex-start">A Frontend focused Web Developer building the Frontend of Websites and</p>
                 </div>
-                <div className="text-end w-full">
+                <div className="flex justify-end text-end flex-end">
                     <IconosHorizontal></IconosHorizontal>
                 </div>
             </div>
 
             <div className="columns-2 mt-1 mb-10">
-                <div className="text-start flex justify-around w-full">
-                <p className="text-start flex justify-around w-full">Web Applications that leads to the success of the overall product</p>
+                <div className="flex justify-start text-start flex-start">
+                <p className="flex justify-start text-start flex-start">Web Applications that leads to the success of the overall product</p>
                 </div>
                 <div className="text-end w-full">
                 </div>
